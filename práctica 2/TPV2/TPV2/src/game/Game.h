659331f05ea27be3a0ec08957b818e6963f60a0f
Paulalemany <palemany@ucm.es>
2024-03-24 13:03:41 +0100

    maquina de estados

--- práctica 2/TPV2/TPV2/src/game/Game.h
+++ práctica 2/TPV2/TPV2/src/game/Game.h
@@ -17,6 +17,32 @@ public:
 	enum State {
 		GAMEOVER, NEWGAME, NEWROUND, PAUSE, RUNNING
 	};
+
+	inline void setState(State s) {
+		GameState* new_state = nullptr;
+		switch (s) {
+		case GAMEOVER:
+			new_state = gameover_state_;
+			break;
+		case NEWGAME:
+			new_state = newgame_state_;
+			break;
+		case NEWROUND:
+			new_state = newround_state_;
+			break;
+		case PAUSE:
+			new_state = paused_state_;
+			break;
+		case RUNNING:
+			new_state = runing_state_;
+			break;
+		default:
+			break;
+		}
+		current_state_->leave();
+		current_state_ = new_state;
+		current_state_->enter();
+	}
 private:
 	ecs::Manager *mngr_;
 	ecs::System *pacmanSys_;
@@ -24,6 +50,13 @@ private:
 	ecs::System *startsSys_;
 	ecs::System *renderSys_;
 	ecs::System *collisionSys_;
+
+	GameState* current_state_;
+	GameState* paused_state_;
+	GameState* runing_state_;
+	GameState* newgame_state_;
+	GameState* newround_state_;
+	GameState* gameover_state_;
 
 };
 
